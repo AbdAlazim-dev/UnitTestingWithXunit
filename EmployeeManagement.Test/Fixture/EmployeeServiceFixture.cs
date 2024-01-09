167a6ea77abcc39fe5e241e9c40555c74bd03697
unknown <abdalazimattya.dev@gmail.com>
2024-01-09 17:01:32 +0300

    Sharing context with class fixture approach

---
+++ EmployeeManagement.Test/Fixture/EmployeeServiceFixture.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.Services.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test.Fixture
+{
+    public class EmployeeServiceFixture : IDisposable
+    {
+        public EmployeeManagementTestDataRepository EmployeeTestDataRepository { get; }
+        public EmployeeService EmployeeService { get; }
+
+        public EmployeeServiceFixture() 
+        {
+            EmployeeTestDataRepository = new EmployeeManagementTestDataRepository();
+            EmployeeService = new EmployeeService(EmployeeTestDataRepository, new EmployeeFactory());
+        }
+
+        public void Dispose()
+        {
+            //clean up
+        }
+    }
+}
