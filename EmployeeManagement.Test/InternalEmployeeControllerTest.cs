5145d3a829e327d76da5997ec40c21d437fa828b
unknown <abdalazimattya.dev@gmail.com>
2024-01-17 14:56:05 +0300

    testig controller that use manual mapping

--- EmployeeManagement.Test/InternalEmployeeControllerTest.cs
+++ EmployeeManagement.Test/InternalEmployeeControllerTest.cs
@@ -13,16 +13,17 @@ namespace EmployeeManagement.Test
     public class InternalEmployeeControllerTest
     {
         private readonly InternalEmployeesController _internalEmployeeController;
-
+        private readonly InternalEmployee _firstEmployee;
         public InternalEmployeeControllerTest() 
         {
+            _firstEmployee = new InternalEmployee("Morgan", "Freman", 2, 2500, false, 1);
             // Arrange 
             var employeeServiceMock = new Mock<IEmployeeService>();
             employeeServiceMock
                 .Setup(m => m.FetchInternalEmployeesAsync())
                 .ReturnsAsync(new List<InternalEmployee>()
                 {
-                    new InternalEmployee("Morgan", "Freman", 2, 2500, false, 1),
+                    _firstEmployee,
                     new InternalEmployee("abdula", "Mohammed", 2, 2500, false, 1),
                     new InternalEmployee("Ahmed", "Rashid", 2, 2500, false, 1)
                 });
@@ -58,6 +59,28 @@ namespace EmployeeManagement.Test
 
             Assert.IsAssignableFrom<IEnumerable<Models.InternalEmployeeDto>>(
                 ((OkObjectResult)actionResult.Result).Value);
+        }
+        [Fact]
+        public async Task GetInternalEmployess_GetAction_IEnumerableOfInternalEmployeeDtoMustBe3()
+        {
+
+            // Act 
+            var result = await _internalEmployeeController.GetInternalEmployees();
+
+            //Assert 
+            var actionResult = Assert
+                .IsType<ActionResult<IEnumerable<Models.InternalEmployeeDto>>>(result);
+
+            var okObjectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+
+            var dto = Assert.IsAssignableFrom<IEnumerable<Models.InternalEmployeeDto>>(okObjectResult.Value);
+
+            var firstEmployee = dto.First();
+            Assert.Equal(_firstEmployee.FullName, firstEmployee.FullName);
+            Assert.Equal(_firstEmployee.FirstName, firstEmployee.FirstName);
+            Assert.Equal(_firstEmployee.LastName, firstEmployee.LastName);
+            Assert.Equal(_firstEmployee.Salary, firstEmployee.Salary);
+            Assert.Equal(_firstEmployee.Id, firstEmployee.Id);
         }
     }
 }
