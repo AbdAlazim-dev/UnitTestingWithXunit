c51fb8e7ee168f933e787853636ebc8b78c43638
unknown <abdalazimattya.dev@gmail.com>
2024-01-17 16:00:28 +0300

    Testing controllers modelState (not the default framework behavior)

---
+++ EmployeeManagement.Test/DemoInternalEmployeeTest.cs
@@ -0,0 +1,41 @@
+﻿
+using AutoMapper;
+using EmployeeManagement.Business;
+using EmployeeManagement.Controllers;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EmployeeManagement.Test
+{
+    public class DemoInternalEmployeeTest
+    {
+        [Fact]
+        public async Task CreateInternalEmployee_InvalidInput_MustReturnBadRequest()
+        {
+            // Arrange
+            var employeeService = new Mock<IEmployeeService>();
+            var mapper = new Mock<IMapper>();
+            var demoInternalEmployeeController = new DemoInternalEmployeeController
+                (employeeService.Object, mapper.Object);
+            //Creating Model instance
+            var internalEmployee = new InternalEmployeeDto();
+            // make it invalid
+            demoInternalEmployeeController.ModelState.AddModelError("FirstName", "Required");
+
+            // Act 
+            var result = await demoInternalEmployeeController
+                .CreateInternalEmployee(internalEmployee);
+            // Assert 
+
+            var actionResult = 
+                Assert.IsType<ActionResult<Models.InternalEmployeeDto>>(result);
+            
+            //Check if it resolve in BadRequest objectResult
+
+            var badRequestObject = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+
+            Assert.IsType<SerializableError>(badRequestObject.Value);
+        }
+    }
+}
---
+++ EmployeeManagement/Controllers/DemoInternalEmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/demoInternalEmployee")]
+    public class DemoInternalEmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IMapper _mapper;
+        public DemoInternalEmployeeController(IEmployeeService employeeService,
+            IMapper mapper)
+        {
+            _employeeService = employeeService;
+            _mapper = mapper;
+        }
+        [HttpPost]
+        public async Task<ActionResult<InternalEmployeeDto>> CreateInternalEmployee(InternalEmployeeDto internalEmployeeDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            // create an internal employee entity with default values filled out
+            // and the values inputted via the POST request
+            var internalEmployee =
+                    await _employeeService.CreateInternalEmployeeAsync(
+                        internalEmployeeDto.FirstName, internalEmployeeDto.LastName);
+
+            // persist it
+            await _employeeService.AddInternalEmployeeAsync(internalEmployee);
+
+            // return created employee after mapping to a DTO
+            return CreatedAtAction("GetInternalEmployee",
+                _mapper.Map<InternalEmployeeDto>(internalEmployee),
+                new { employeeId = internalEmployee.Id });
+        }
+    }
+}
