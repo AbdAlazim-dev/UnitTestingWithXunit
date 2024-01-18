f7cb9af25314d0da5a1a8642759408fc87e97207
unknown <abdalazimattya.dev@gmail.com>
2024-01-18 14:53:10 +0300

    Testing a custom middleware

---
+++ EmployeeManagement.Test/TestEmployeeManagementSecurityHeadersMiddleware.cs
@@ -0,0 +1,30 @@
+﻿
+using EmployeeManagement.Middleware;
+using Microsoft.AspNetCore.Http;
+
+namespace EmployeeManagement.Test
+{
+    public class TestEmployeeManagementSecurityHeadersMiddleware
+    {
+        [Fact]
+        public async Task InvokeAsync_Invoke_TheHeadersMustBeAddedToTheResponseHeader()
+        {
+            // Arrange
+            var httpContext = new DefaultHttpContext();
+            RequestDelegate next = (HttpContext httpContext) => Task.CompletedTask;
+
+            var middleware = new EmployeeManagementSecurityHeadersMiddleware(next);
+
+
+            // Act
+            await middleware.InvokeAsync(httpContext);
+
+            var firstHeader = httpContext.Response.Headers["Content-Security-Policy"];
+            var secondHeader = httpContext.Response.Headers["X-Content-Type-Options"];
+            // Assert
+
+            Assert.Equal("default-src 'self';frame-ancestors 'none';", firstHeader);
+            Assert.Equal("nosniff", secondHeader);
+        }
+    }
+}
